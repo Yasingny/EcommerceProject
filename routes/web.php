5ba09a03fd621cc990309d12b0bd83d008e0115b
Yasingny <gefwert97@hotmail.com>
2023-09-11 01:45:47 +0300

    ecommerce development

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\Controllersx\HomeController;
+use App\Http\Controllers\Controllersx\KategoriController;
+use App\Http\Controllers\Controllersx\KullaniciController;
+use App\Http\Controllers\Controllersx\OdemeController;
+use App\Http\Controllers\Controllersx\SepetController;
+use App\Http\Controllers\Controllersx\SiparisController;
+use App\Http\Controllers\Controllersx\UrunController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +20,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+//Route yapılanmasında Countroller kullanımı
+//Route::get('/',[HomeController::class,'index']);
+
+Route::get('/',[HomeController::class,'index'])->name('anasayfa');
+
+Route::get('/kategori/{slug_kategori_adi}',[KategoriController::class,'index'])->name('kategori');
+
+Route::get('/urun/{slug_uruni_adi}',[UrunController::class,'index'])->name('urun');
+
+Route::get('/sepet/',[SepetController::class,'index'])->name('sepet');
+
+Route::get('/odeme/',[OdemeController ::class,'index'])->name('odeme');
+
+Route::get('/siparisler',[siparisController::class,'index'])->name('siparisler');
+
+Route::get('/siparisler/{id}',[siparisController::class,'detay'])->name('siparis');
+
+Route::group(['prefix'=>'/kullanici'], function (){
+    Route::get('kullanici/oturumac',[KullaniciController::class,'giris_form'])->name('kullanici.oturumac');
+    Route::get('kullanici/kaydol',[KullaniciController::class,'kayit_form'])->name('kullanici.kaydol');
 });
+
+
+
+
